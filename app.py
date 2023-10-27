8ad3325305edb2390d51f19add164bf4a264602b
Sk Sameer Salam <113439449+dampdigits@users.noreply.github.com>
2023-10-27 07:16:27 +0000

    page seeding added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """ Module to create flask application """
 
-from flask import Flask, render_template
+import random
+from flask import Flask, render_template, request
 from helpers import random_string
 
 # Create Flask application
@@ -8,9 +9,14 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 """ Define default route"""
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET","POST"])
 def index():
     """ Display gibberish text """
+    page = 0
+
+    if request.method == "POST":
+        page = request.form.get("page")
+    random.seed(page)
     length = 2000
     text = random_string(length)
     return render_template("index.html", text=text)
